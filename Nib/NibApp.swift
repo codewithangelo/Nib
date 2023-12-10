331b1382da11e5b20bca0cc9215e7ecd31224dec
codewithangelo <codewithangelo@gmail.com>
2023-12-10 18:35:06 -0500

    Initial starter code

---
+++ Nib/NibApp.swift
@@ -0,0 +1,17 @@
+//
+//  NibApp.swift
+//  Nib
+//
+//  Created by Angelo Austria on 2023-12-10.
+//
+
+import SwiftUI
+
+@main
+struct NibApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
