7a759061ff8ac54ca8553bdb78665e538230f639
codewithangelo <codewithangelo@gmail.com>
2023-12-10 18:50:25 -0500

    Add GoogleSignInResult and AppleSignInResult models

---
+++ Nib/Models/IdentityProviderModels/AppleSignInResult.swift
@@ -0,0 +1,13 @@
+//
+//  AppleSignInResult.swift
+//  Nib
+//
+//  Created by Angelo Austria on 2023-12-10.
+//
+
+import Foundation
+
+struct SignInWithAppleResult {
+    let token: String
+    let nonce: String
+}
---
+++ Nib/Models/IdentityProviderModels/GoogleSignInResult.swift
@@ -0,0 +1,13 @@
+//
+//  GoogleSignInResult.swift
+//  Nib
+//
+//  Created by Angelo Austria on 2023-12-10.
+//
+
+import Foundation
+
+struct GoogleSignInResult {
+    let idToken: String
+    let accessToken: String
+}
