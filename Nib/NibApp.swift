b97c48cb27ab50f27958b213ae4949b1e7759850
codewithangelo <codewithangelo@gmail.com>
2023-12-10 18:58:53 -0500

    Configure Firebase in app root view

--- Nib/NibApp.swift
+++ Nib/NibApp.swift
@@ -5,13 +5,24 @@
 //  Created by Angelo Austria on 2023-12-10.
 //
 
+import Firebase
 import SwiftUI
 
 @main
 struct NibApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+
+        return true
     }
 }
