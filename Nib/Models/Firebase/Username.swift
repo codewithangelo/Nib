d264f80e75b204c66a7a53470cfa678e879c7c4e
codewithangelo <codewithangelo@gmail.com>
2023-12-10 20:20:22 -0500

    Add Username model again

--- Nib/Models/Firebase/Username.swift
+++ Nib/Models/Firebase/Username.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+struct Username: Codable {
+    let userId: String
+    let username: String
+    
+    init(
+        userId: String,
+        username: String
+    ) {
+        self.userId = userId
+        self.username = username
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case userId = "user_id"
+        case username = "username"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.userId = try container.decode(String.self, forKey: .userId)
+        self.username = try container.decode(String.self, forKey: .username)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.userId, forKey: .userId)
+        try container.encode(self.username, forKey: .username)
+    }
+}
