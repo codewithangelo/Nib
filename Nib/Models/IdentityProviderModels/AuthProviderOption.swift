959304911660328b2a765f73f551da67193c9529
codewithangelo <codewithangelo@gmail.com>
2023-12-10 19:17:09 -0500

    Add AuthProviderOption enum

---
+++ Nib/Models/IdentityProviderModels/AuthProviderOption.swift
@@ -0,0 +1,14 @@
+//
+//  AuthProviderOption.swift
+//  Nib
+//
+//  Created by Angelo Austria on 2023-12-10.
+//
+
+import Foundation
+
+enum AuthProviderOption: String {
+    case email = "password"
+    case google = "google.com"
+    case apple = "apple.com"
+}
