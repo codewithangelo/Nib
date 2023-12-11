d4bbd7b1b0aa36de6d0bff573ae3d6b5c4ba5faf
codewithangelo <codewithangelo@gmail.com>
2023-12-10 21:48:40 -0500

    Add shared TextInput component

---
+++ Nib/Features/UserProfile/Views/Settings/Username/UsernameView.swift
@@ -0,0 +1,18 @@
+//
+//  UsernameView.swift
+//  Nib
+//
+//  Created by Angelo Austria on 2023-12-10.
+//
+
+import SwiftUI
+
+struct UsernameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UsernameView()
+}
---
+++ Nib/Features/UserProfile/Views/Settings/Username/UsernameViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UsernameViewModel.swift
+//  Nib
+//
+//  Created by Angelo Austria on 2023-12-10.
+//
+
+import Foundation
