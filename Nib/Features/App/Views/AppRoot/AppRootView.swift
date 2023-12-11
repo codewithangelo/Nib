2ce0bfb49c6069c76f2dea04781220b047bdf943
codewithangelo <codewithangelo@gmail.com>
2023-12-10 20:48:09 -0500

    Load user data on app start

--- Nib/Features/App/Views/AppRoot/AppRootView.swift
+++ Nib/Features/App/Views/AppRoot/AppRootView.swift
@@ -9,25 +9,47 @@ import SwiftUI
 
 struct AppRootView: View {
     private static let authenticationService: NibAuthenticationServiceProtocol = NibAuthenticationService()
+    private static let usernService: UserServiceProtocol = UserService()
     
     @StateObject
-    var viewModel: AppRootViewModel = AppRootViewModel(authenticationService: authenticationService)
+    var viewModel: AppRootViewModel = AppRootViewModel(
+        authenticationService: authenticationService,
+        userService: usernService
+    )
     
     var body: some View {
         ZStack {
             if (!viewModel.showSignInView) {
-                YourProfileView()
-                    .environmentObject(viewModel)
+                TabView {
+                    YourProfileView()
+                        .tabItem { Image(systemName: "person.crop.circle") }
+                        .environmentObject(viewModel)
+                }
+                .onAppear(perform: loadCurrentUserData)
             }
         }
-        .onAppear {
-            viewModel.loadCurrentUser()
-        }
+        .onAppear(perform: checkIsUserAuthenticated)
         .fullScreenCover(isPresented: $viewModel.showSignInView) {
             SignInView(
                 onSignInCompleted: { viewModel.showSignInView = false },
                 onSignInError: { }
             )
+        }
+    }
+}
+
+extension AppRootView {
+    private func checkIsUserAuthenticated() {
+        viewModel.checkIsUserAuthenticated()
+    }
+    
+    private func loadCurrentUserData() {
+        Task {
+            do {
+                try await viewModel.loadCurrentUserData()
+            } catch {
+                print(error)
+            }
         }
     }
 }
--- Nib/Features/App/Views/AppRoot/AppRootViewModel.swift
+++ Nib/Features/App/Views/AppRoot/AppRootViewModel.swift
@@ -8,18 +8,39 @@
 import Foundation
 
 @MainActor
-final class AppRootViewModel: ObservableObject {    
+final class AppRootViewModel: ObservableObject {
+    @Published
+    var currentUser: User? = nil
+    
     @Published
     var showSignInView: Bool = false
     
     private let authenticationService: NibAuthenticationServiceProtocol
+    private let userService: UserServiceProtocol
     
-    init(authenticationService: NibAuthenticationServiceProtocol) {
+    init(
+        authenticationService: NibAuthenticationServiceProtocol,
+        userService: UserServiceProtocol
+    ) {
         self.authenticationService = authenticationService
+        self.userService = userService
     }
     
-    func loadCurrentUser() {
+    func checkIsUserAuthenticated() {
         let authUser = try? authenticationService.getAuthenticatedUser()
         self.showSignInView = authUser == nil
+    }
+    
+    func loadCurrentUserData() async throws {
+        let authUser = try authenticationService.getAuthenticatedUser()
+        
+        do {
+            let currentUser = try await userService.getUser(userId: authUser.uid)
+            self.currentUser = currentUser
+        } catch {
+            let newUser = User(authDataResult: authUser)
+            try await userService.createNewUser(user: newUser)
+            self.currentUser = newUser
+        }
     }
 }
