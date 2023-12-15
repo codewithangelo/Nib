966d04984ba9c5093d3f4a294e799df1c45d5838
codewithangelo <codewithangelo@gmail.com>
2023-12-15 17:50:17 -0500

    Add Toast shared component

---
+++ Nib/Features/Shared/Components/Toast/Toast.swift
@@ -0,0 +1,15 @@
+//
+//  ToastModel.swift
+//  Nib
+//
+//  Created by Angelo Austria on 2023-12-14.
+//
+
+import Foundation
+
+struct Toast: Equatable {
+  var style: ToastStyle
+  var message: String
+  var duration: Double = 3
+  var width: Double = .infinity
+}
---
+++ Nib/Features/Shared/Components/Toast/ToastModifier.swift
@@ -0,0 +1,67 @@
+//
+//  ToastModifier.swift
+//  Nib
+//
+//  Created by Angelo Austria on 2023-12-14.
+//
+import SwiftUI
+
+struct ToastModifier: ViewModifier {
+    
+    @Binding var toast: Toast?
+    @State private var workItem: DispatchWorkItem?
+    
+    func body(content: Content) -> some View {
+        content
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .overlay(
+                ZStack {
+                    mainToastView()
+                        .offset(y: 32)
+                        .onAppear(perform: showToast)
+                }.animation(.spring(), value: toast)
+            )
+    }
+    
+    @ViewBuilder func mainToastView() -> some View {
+        if let toast = toast {
+            VStack {
+                ToastView(
+                    style: toast.style,
+                    message: toast.message,
+                    width: toast.width
+                ) {
+                    dismissToast()
+                }
+                Spacer()
+            }
+        }
+    }
+    
+    private func showToast() {
+        guard let toast = toast else { return }
+        
+        UIImpactFeedbackGenerator(style: .light)
+            .impactOccurred()
+    
+        if toast.duration > 0 {
+            workItem?.cancel()
+            
+            let task = DispatchWorkItem {
+                dismissToast()
+            }
+            print(task)
+            workItem = task
+            DispatchQueue.main.asyncAfter(deadline: .now() + toast.duration, execute: task)
+        }
+    }
+    
+    private func dismissToast() {
+        withAnimation {
+            toast = nil
+        }
+        
+        workItem?.cancel()
+        workItem = nil
+    }
+}
