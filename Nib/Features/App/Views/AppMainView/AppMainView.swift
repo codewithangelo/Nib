6720495312bed98dffbb7b45571f7e76ece9ba0b
codewithangelo <codewithangelo@gmail.com>
2023-12-15 17:52:48 -0500

    Create AppMainView

---
+++ Nib/Features/App/Views/AppMainView/AppMainView.swift
@@ -0,0 +1,72 @@
+//
+//  AppMainView.swift
+//  Nib
+//
+//  Created by Angelo Austria on 2023-12-15.
+//
+
+import SwiftUI
+
+struct AppMainView: View {
+    @EnvironmentObject
+    var appRoot: AppRootViewModel
+    
+    private static let authenticationService: NibAuthenticationServiceProtocol = NibAuthenticationService()
+    private static let userService: UserServiceProtocol = UserService()
+    
+    @StateObject
+    private var viewModel: AppMainViewModel = AppMainViewModel(
+        authenticationService: authenticationService,
+        userService: userService
+    )
+    
+    var body: some View {
+        VStack {
+            switch viewModel.state {
+            case .unset:
+                EmptyView()
+            case .loading:
+                ProgressView()
+            case .error:
+                Text("app.main.unableToGetCurrentUser")
+            case .success:
+                TabView(selection: $viewModel.tabSelection) {
+                    PoemFeedView()
+                        .tag(AppMainViewModel.Tab.home)
+                        .tabItem { Image(systemName: "house") }
+                    
+                    DraftView()
+                        .tag(AppMainViewModel.Tab.publisher)
+                        .tabItem { Image(systemName: "plus.app") }
+                        .environmentObject(viewModel)
+                    
+                    YourProfileView()
+                        .tag(AppMainViewModel.Tab.userProfile)
+                        .tabItem { Image(systemName: "person.crop.circle") }
+                        .environmentObject(appRoot)
+                        .environmentObject(viewModel)
+                }
+            }
+        }
+        .onAppear(perform: loadCurrentUserData)
+    }
+}
+
+extension AppMainView {
+    private func loadCurrentUserData() {
+        Task {
+            do {
+                try await viewModel.loadCurrentUserData()
+            } catch {
+                // Silently fail
+            }
+        }
+    }
+}
+
+#Preview {
+    AppMainView()
+        .environmentObject(
+            AppRootViewModel(authenticationService: NibAuthenticationService())
+        )
+}
---
+++ Nib/Features/App/Views/AppMainView/AppMainViewModel.swift
@@ -0,0 +1,85 @@
+//
+//  AppMainViewModel.swift
+//  Nib
+//
+//  Created by Angelo Austria on 2023-12-15.
+//
+
+import Foundation
+
+@MainActor
+final class AppMainViewModel: ObservableObject {
+    enum AppMainViewModelError: LocalizedError {
+        case unableToGetCurrentUser
+        
+        var errorDescription: String? {
+            switch self {
+            case .unableToGetCurrentUser:
+                return NSLocalizedString("app.main.unableToGetCurrentUser", comment: "")
+            }
+        }
+    }
+    
+    enum State: Equatable {
+        case unset
+        case loading
+        case error(error: String?)
+        case success(currentUser: User)
+    }
+    
+    @Published
+    var state: State = .unset
+    
+    @Published
+    var currentUser: User? = nil
+    
+    enum Tab {
+        case home
+        case publisher
+        case userProfile
+    }
+    
+    @Published
+    var tabSelection: Tab = .home
+    
+    private let authenticationService: NibAuthenticationServiceProtocol
+    private let userService: UserServiceProtocol
+    
+    init(
+        authenticationService: NibAuthenticationServiceProtocol,
+        userService: UserServiceProtocol
+    ) {
+        self.authenticationService = authenticationService
+        self.userService = userService
+    }
+    
+    func loadCurrentUserData() async throws {
+        guard let authUser = try? authenticationService.getAuthenticatedUser() else {
+            return
+        }
+        
+        self.state = .loading
+        
+        do {
+            guard let currentUser = try? await userService.getUser(userId: authUser.uid) else {
+                throw AppMainViewModelError.unableToGetCurrentUser
+            }
+            self.state = .success(currentUser: currentUser)
+            self.currentUser = currentUser
+        } catch {
+            try await createNewUser(authUser: authUser)
+        }
+    }
+    
+    private func createNewUser(authUser: AuthDataResult) async throws {
+        do {
+            let newUser = User(authDataResult: authUser)
+            try await userService.createNewUser(user: newUser)
+            self.state = .success(currentUser: newUser)
+            self.currentUser = newUser
+        } catch {
+            self.state = .error(error: AppMainViewModelError.unableToGetCurrentUser.errorDescription)
+            throw AppMainViewModelError.unableToGetCurrentUser
+        }
+    }
+}
